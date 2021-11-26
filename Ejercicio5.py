cf5f1705e9de4d545e7d29135502c721be66ea17
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-26 18:42:14 +0100

    hecho

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,3 +1,8 @@
 """Escriba una función recursiva para calcular la secuencia de Fibonacci.
 ¿Cómo se compara el desempeño de la función recursiva con el de una versión iterativa?
 """
+def fibonacci(numero):
+    if (numero==0 or numero==1):
+        return 1
+    else:
+        return (fibonacci(numero-1)+fibonacci(numero-2))
