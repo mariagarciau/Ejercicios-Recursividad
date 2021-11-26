ea4755c3960604def765de2e372b2eacf7618f11
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-11-26 19:05:53 +0100

    hanoi

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -1,3 +1,11 @@
 """Implemente una solución al problema de las torres de Hanoi utilizando tres pilas
 para mantener un seguimiento de los discos.
 """
+def torres_Hanoi(discos,TorreOrigen=1,TorreAuxiliar=2,TorreDestino=3):
+    if discos==1:
+        print (TorreOrigen,"a",TorreDestino)
+    else:
+        torres_Hanoi(discos-1,TorreOrigen,TorreDestino,TorreAuxiliar)
+        print(TorreOrigen,"a",TorreDestino)
+        torres_Hanoi(discos-1,TorreAuxiliar,TorreOrigen,TorreDestino)
+    return
